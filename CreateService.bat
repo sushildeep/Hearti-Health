b8880246d2b8ed4d14f73b087dde5ee7fbb672b7
saritha1919 <47768827+saritha1919@users.noreply.github.com>
2020-01-30 09:57:24 +0530

    Update CreateService.bat

--- CreateService.bat
+++ CreateService.bat
@@ -1,7 +1,6 @@
 net start | find "HeartiHealthApp"
 if ERRORLEVEL 1 sc stop HeartiHealthApp
 sc delete "HeartiHealthApp"
-cd "C:\Program Files (x86)\Jenkins\workspace\Hearti-Health-Services"
 start WinSW.NET4.exe install
 sc start "HeartiHealthApp"
 pause
