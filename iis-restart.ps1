ff28a8d29c5555661d28a5704a6ab0b1b554e61d
saritha1919 <47768827+saritha1919@users.noreply.github.com>
2020-01-30 14:44:31 +0530

    Update iis-restart.ps1

--- iis-restart.ps1
+++ iis-restart.ps1
@@ -1,3 +1,3 @@
-$website = "http://172.30.12.171:9091"
-Start-WebSite -Name "$Test-Angular"
-Start-WebAppPool -Name "$Test-Angular"
+$website = "Test-Angular"
+Start-WebSite -Name "$website"
+Start-WebAppPool -Name "$website"
