a2637f7a58746d0e0e905f0d126f22ed45d87b8f
saritha1919 <47768827+saritha1919@users.noreply.github.com>
2020-01-30 13:06:21 +0530

    Update iis-restart.ps1

--- iis-restart.ps1
+++ iis-restart.ps1
@@ -1,3 +1,3 @@
-$website = "GT-ComplyRite_MasterData"
-Start-WebSite -Name "$website"
-Start-WebAppPool -Name "$website"
+$website = "http://172.30.12.171:9091"
+Start-WebSite -Name "$Test-Angular"
+Start-WebAppPool -Name "$Test-Angular"
